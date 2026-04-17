c87905abb01bb5e604f5d54087777d5244020976
Patt MacMillan <patt.macmillan@velir.com>
2026-04-17 12:20:50 -0500

    Fixed suspense issue with article search.

--- apps/news/src/app/search/page.tsx
+++ apps/news/src/app/search/page.tsx
@@ -1,5 +1,6 @@
 import { ArticleSearch } from "@/components/article-search";
 import { getCategories } from "../lib/categories";
+import { Suspense } from "react";
 
 export default async function SearchPage() {
   //const articles = await searchArticles(6);
@@ -9,7 +10,9 @@ export default async function SearchPage() {
     <div className="min-h-screen flex flex-col">
       <main className="flex-1">
         <div>
-          <ArticleSearch categories={categories} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <ArticleSearch categories={categories} />
+          </Suspense>
         </div>
       </main>
     </div>
