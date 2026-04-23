ba5509cec3a4074f5ac41b3a01604bd3d58ed469
Patt MacMillan <patt.macmillan@velir.com>
2026-04-23 12:01:12 -0500

    Temporarily disabled trending articles on article page.

--- apps/news/src/app/articles/[slug]/page.tsx
+++ apps/news/src/app/articles/[slug]/page.tsx
@@ -34,12 +34,12 @@ export async function generateMetadata({ params }: Props) {
   };
 }
 
-export default async function ArticlePage({ params }: Props) {
-  const trendingArticles = await getTrendingArticles(4);
+export default function ArticlePage({ params }: Props) {
+  // const trendingArticles = await getTrendingArticles(4);
   return (
     <Suspense fallback={<ArticleSkeleton />}>
       <Article params={params} />
-      <TrendingArticles articles={trendingArticles} />
+      {/* <TrendingArticles articles={trendingArticles} /> */}
     </Suspense>
   );
 }
