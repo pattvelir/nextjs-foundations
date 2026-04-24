e090453455f67173ddfdb6a5839b3db1b7190da1
Patt MacMillan <patt.macmillan@velir.com>
2026-04-23 20:47:20 -0500

    Adjusted 404 logic.

--- apps/news/src/app/lib/article.ts
+++ apps/news/src/app/lib/article.ts
@@ -4,15 +4,23 @@ import { cacheLife } from "next/cache";
 
 export async function getArticleBySlug(slug: string): Promise<Article | null> {
   "use cache";
+  try {
+    const article = await apiFetch<Article>(`/articles/${slug}`);
 
-  const article = await apiFetch<Article>(`/articles/${slug}`);
+    if (!article) {
+      return null;
+    }
 
-  if (!article) {
-    return null;
+    // We'll only cache the article if it was successfully found.
+    cacheLife("articles");
+
+    return ArticleSchema.parse(article);
+  } catch (err: unknown) {
+    console.log("error:", err);
+    if (err instanceof Error && err.message === "NOT_FOUND") {
+      return null;
+    }
+
+    throw err;
   }
-
-  // We'll only cache the article if it was successfully found.
-  cacheLife("articles");
-
-  return ArticleSchema.parse(article);
 }
